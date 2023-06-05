e831f57b3bc74773376c8ea1445a7e4ec6334127
REDitor <sander@trone.nl>
2023-06-05 20:53:58 +0200

    GetResults endpoint working

--- chaincode/lib/egg-tracking-contract.js
+++ chaincode/lib/egg-tracking-contract.js
@@ -3,313 +3,326 @@
  * Author: MFK
  */
 
-'use strict';
+"use strict";
 
 //import Hyperledger Fabric SDK
-const { Contract } = require('fabric-contract-api');
-const Poll = require('./model/poll.js');
-const Vote = require('./model/vote.js');
+const { Contract } = require("fabric-contract-api");
+const Poll = require("./model/poll.js");
+const Vote = require("./model/vote.js");
 
 // import Participant
-const Participant = require('./model/participant.js');
+const Participant = require("./model/participant.js");
 
 class EggTrackingContract extends Contract {
+	counter = 0;
 
-  counter = 0;
+	/**
+	 * Utility function checking if a user is an admin
+	 * @param {*} idString - the identity object
+	 */
+	isAdmin(identity) {
+		var match = identity.getID().match(".*CN=(.*)::");
+		return match !== null && match[1] === "admin";
+	}
 
-  /**
-   * Utility function checking if a user is an admin
-   * @param {*} idString - the identity object
-   */
-  isAdmin(identity) {
-    var match = identity.getID().match('.*CN=(.*)::');
-    return match !== null && match[1] === 'admin';
-  }
+	/**
+	 * Utility function checking if a user is a farmer
+	 * @param {*} identity - the identity object
+	 */
+	isVoter(identity) {
+		return identity.assertAttributeValue("role", "Voter");
+	}
 
-  /**
-   * Utility function checking if a user is a farmer
-   * @param {*} identity - the identity object
-   */
-  isVoter(identity) {
-    return identity.assertAttributeValue('role', 'Voter');
-  }
+	/**
+	 * Utility function checking if a user is a distributor
+	 * @param {*} identity - the identity object
+	 */
+	isOrganizer(identity) {
+		return identity.assertAttributeValue("role", "Organizer");
+	}
 
-  /**
-   * Utility function checking if a user is a distributor
-   * @param {*} identity - the identity object
-   */
-  isOrganizer(identity) {
-    return identity.assertAttributeValue('role', 'Organizer');
-  }
+	/**
+	 * Utility function to get the id of the participant
+	 * @param {*} id - the id of the participant
+	 */
+	getParticipantId(identity) {
+		return identity.getAttributeValue("id");
+	}
 
-  /**
-   * Utility function to get the id of the participant
-   * @param {*} id - the id of the participant
-   */
-  getParticipantId(identity) {
-    return identity.getAttributeValue('id');
-  }
+	/**
+	 *
+	 * assetExists
+	 *
+	 * Checks to see if a key exists in the world state.
+	 * @param assetId - the key of the asset to read
+	 * @returns boolean indicating if the asset exists or not.
+	 */
+	async assetExists(ctx, assetId) {
+		const buffer = await ctx.stub.getState(assetId);
+		return !!buffer && buffer.length > 0;
+	}
 
-  /**
-   *
-   * assetExists
-   *
-   * Checks to see if a key exists in the world state. 
-   * @param assetId - the key of the asset to read
-   * @returns boolean indicating if the asset exists or not. 
-   */
-  async assetExists(ctx, assetId) {
+	/**
+	 * Create Participant
+	 *
+	 * This transaction is started by the participant during sign-up
+	 *
+	 * @param id - The participant identifier
+	 * @param name - The participant name
+	 * @param role - Farmer, Shipper, or Distributor
+	 * @returns the newly created participant
+	 */
+	async createParticipant(ctx, id, name, role) {
+		let identity = ctx.clientIdentity;
 
-    const buffer = await ctx.stub.getState(assetId);
-    return (!!buffer && buffer.length > 0);
-  }
+		if (!this.isAdmin(identity)) {
+			throw new Error(`Only administrators can create participants`);
+		}
 
-  /**
-   * Create Participant
-   * 
-   * This transaction is started by the participant during sign-up
-   *     
-   * @param id - The participant identifier
-   * @param name - The participant name
-   * @param role - Farmer, Shipper, or Distributor
-   * @returns the newly created participant
-   */
-  async createParticipant(ctx, id, name, role) {
+		// Generate a participant representation
+		let participant = new Participant(id, name, role);
 
-    let identity = ctx.clientIdentity;
+		// generate the key for the participant
+		let key = participant.getType() + ":" + participant.getId();
 
-    if (!this.isAdmin(identity)) {
-      throw new Error(`Only administrators can create participants`);
-    }
+		// check if the participant already exists
+		let exists = await this.assetExists(ctx, key);
 
-    // Generate a participant representation
-    let participant = new Participant(id, name, role);
+		if (exists) {
+			throw new Error(`Participant with id ${key} already exists`);
+		}
 
-    // generate the key for the participant
-    let key = participant.getType() + ":" + participant.getId();
+		// update state with new participant
+		await ctx.stub.putState(key, participant.serialise());
 
-    // check if the participant already exists
-    let exists = await this.assetExists(ctx, key);
+		// Return the newly created shipment
+		return JSON.stringify(participant);
+	}
 
-    if (exists) {
-      throw new Error(`Participant with id ${key} already exists`);
-    }
+	/**
+	 * Get participant
+	 *
+	 * This transaction is started by the farmer that collected eggs
+	 * and stored them in a box
+	 *
+	 * @param id - The participant identifier
+	 * @returns the participant
+	 */
+	async getParticipant(ctx, id) {
+		let identity = ctx.clientIdentity;
 
-    // update state with new participant
-    await ctx.stub.putState(key, participant.serialise())
+		if (!id === this.getParticipantId(identity) && !this.isAdmin(identity)) {
+			throw new Error(
+				`Only administrators can query other participants. Regular participants can get information of their own account`
+			);
+		}
 
-    // Return the newly created shipment
-    return JSON.stringify(participant);
-  }
+		// get participant
+		const buffer = await ctx.stub.getState("Participant:" + id);
 
-  /**
-   * Get participant
-   * 
-   * This transaction is started by the farmer that collected eggs
-   * and stored them in a box
-   *     
-   * @param id - The participant identifier
-   * @returns the participant
-   */
-  async getParticipant(ctx, id) {
+		// if participant was not found
+		if (!buffer || buffer.length == 0) {
+			throw new Error(`Participant with id ${id} was not found`);
+		}
 
-    let identity = ctx.clientIdentity;
+		// get object from buffer
+		const participant = Participant.deserialise(buffer);
 
-    if (!id === this.getParticipantId(identity) && !this.isAdmin(identity)) {
-      throw new Error(`Only administrators can query other participants. Regular participants can get information of their own account`);
-    }
+		// Return the newly created eggbox
+		return JSON.stringify(participant);
+	}
 
-    // get participant
-    const buffer = await ctx.stub.getState('Participant:'+id);
+	/**
+	 * Report Results
+	 *
+	 * This transaction is used to report the results of a poll.
+	 *
+	 * @param ctx - The transaction context object
+	 * @param pollID - The ID of the poll
+	 * @returns The results of the poll
+	 */
+	async reportResults(ctx, pollID) {
+		let identity = ctx.clientIdentity;
 
-    // if participant was not found
-    if (!buffer || buffer.length == 0) {
-      throw new Error(`Participant with id ${id} was not found`);
-    }
+		// Check if the participant is an organizer
+		if (!this.isOrganizer(identity)) {
+			throw new Error("Only organizers can report poll results");
+		}
 
-    // get object from buffer
-    const participant = Participant.deserialise(buffer);
+		// Get the poll from the world state
+		const pollBuffer = await ctx.stub.getState(pollID);
 
-    // Return the newly created eggbox
-    return JSON.stringify(participant);
-  }
+		if (!pollBuffer || pollBuffer.length === 0) {
+			throw new Error(`Poll with ID ${pollID} does not exist`);
+		}
 
-  
-  /**
-   * Report Results
-   *
-   * This transaction is used to report the results of a poll.
-   *
-   * @param ctx - The transaction context object
-   * @param pollID - The ID of the poll
-   * @returns The results of the poll
-   */
-  async reportResults(ctx, pollID) {
-    let identity = ctx.clientIdentity;
+		const poll = JSON.parse(pollBuffer);
+		const options = poll.options;
+		console.log(`OPTIONS:\n${options}`); // TEST
 
-    // Check if the participant is an organizer
-    if (!this.isOrganizer(identity)) {
-      throw new Error("Only organizers can report poll results");
-    }
+		// Prepare the query to retrieve the votes for the given poll
+		let queryString = {
+			selector: {
+				type: "Vote",
+				poll_ID: pollID,
+			},
+		};
 
-    // Get the poll from the world state
-    const pollBuffer = await ctx.stub.getState(pollID);
+		// Execute the query to retrieve the votes for the given poll
+		const resultsIterator = await ctx.stub.getQueryResult(JSON.stringify(queryString));
 
-    if (!pollBuffer || pollBuffer.length === 0) {
-      throw new Error(`Poll with ID ${pollID} does not exist`);
-    }
+		let resultSummary = {};
 
-    const poll = JSON.parse(pollBuffer.toString());
+		// Process the votes and summarize the results
+		while (true) {
+			const res = await resultsIterator.next();
+			console.log("---------- while(true) ---------"); // TEST
+			console.log("res", res); // TEST
 
-    // Prepare the query to retrieve the votes for the given poll
-    let queryString = {
-        selector: {
-          type: 'Vote',
-          Poll_ID: pollID
-        }
-      };
+			if (res.value && res.value.value) {
+				const vote = JSON.parse(res.value.value);
+				console.log(`VOTE:\n${vote}`);
+				console.log(`SELECTION:\n${vote.selection}`);
 
-    // Execute the query to retrieve the votes for the given poll
-    const resultsIterator = await ctx.stub.getQueryResult(
-      JSON.stringify(queryString)
-    );
+				const selection = options[vote.selection];
 
-    let resultSummary = {};
+				if (resultSummary[selection]) {
+					resultSummary[selection].count++; // Increment the count for this option
+					resultSummary[selection].votes.push(vote); // Store the entire vote object
+				} else {
+					resultSummary[selection] = {
+						count: 1, // Initialize the count
+						votes: [vote], // Create a new array with the vote object
+					};
+				}
+			}
 
-    // Process the votes and summarize the results
-    while (true) {
-      const res = await resultsIterator.next();
-      console.log("---------- while(true) ---------")
-      console.log("res", res)
+			if (res.done) {
+				await resultsIterator.close();
+				break;
+			}
+		}
 
-      if (res.value && res.value.value.toString()) {
-        const vote = JSON.parse(res.value.value.toString());
-        console.log("vote", vote)
+		// Find the option with the highest count (winner)
+		let winner = "";
+		let highestCount = 0;
+		for (const option in resultSummary) {
+			const count = resultSummary[option].count;
+			if (count > highestCount) {
+				highestCount = count;
+				winner = option;
+			}
+		}
 
-        if (resultSummary[poll.options[vote.selection]]) {
-          resultSummary[poll.options[vote.selection]]++;
-        } else {
-          resultSummary[poll.options[vote.selection]] = 1;
-        }
-      }
+		// Prepare the report object
+		const report = {
+			pollID,
+			winner,
+			resultSummary,
+		};
 
-      if (res.done) {
-        await resultsIterator.close();
-        break;
-      }
-    }
+		return report;
+	}
 
-    // Prepare the report object
-    const report = {
-      pollID,
-      resultSummary,
-    };
+	/**
+	 * Create Vote
+	 *
+	 * This transaction is used to create a new vote.
+	 *
+	 * @param ctx - The transaction context object
+	 * @param poll_ID - The poll_ID of the vote
+	 * @param voteTimestamp - The timestamp of the vote
+	 * @param selection - The selected option
+	 * @returns The created vote
+	 */
+	async createVote(ctx, poll_ID, voteTimestamp, selection) {
+		let identity = ctx.clientIdentity;
 
-    return report;
-  }
+		// Check if the participant is a voter
+		if (!this.isVoter(identity)) {
+			throw new Error("Only voters can create votes");
+		}
 
-  /**
-   * Create Vote
-   *
-   * This transaction is used to create a new vote.
-   *
-   * @param ctx - The transaction context object
-   * @param poll_ID - The poll_ID of the vote
-   * @param voteTimestamp - The timestamp of the vote
-   * @param selection - The selected option
-   * @returns The created vote
-   */
-  async createVote(ctx, poll_ID, voteTimestamp, selection) {
-    let identity = ctx.clientIdentity;
-  
-    // Check if the participant is a voter
-    if (!this.isVoter(identity)) {
-      throw new Error("Only voters can create votes");
-    }
-  
-    // Get the participant ID
-    const participantId = identity.getAttributeValue("id");
-  
-    if (!participantId) {
-      throw new Error("Participant ID not found");
-    }
-  
-    // Get the poll from the world state
-    const pollBuffer = await ctx.stub.getState(poll_ID);
-  
-    if (!pollBuffer || pollBuffer.length === 0) {
-      throw new Error(`Poll with ID ${poll_ID} does not exist`);
-    }
-  
-    const poll = JSON.parse(pollBuffer.toString());
-  
-    // Check if the poll is open
-    const currentTimestamp = new Date().getTime();
-    const openTimestamp = new Date(poll.open).getTime();
-    const closedTimestamp = new Date(poll.closed).getTime();
-  
-    if (currentTimestamp < openTimestamp) {
-      throw new Error("The poll is not open yet");
-    }
-  
-    if (currentTimestamp > closedTimestamp) {
-      throw new Error("The poll is closed");
-    }
-  
-    // Generate a unique vote ID
-    const voteID = "vote:" + this.counter;
-  
-    // Create the vote object
-    const vote = new Vote(
-      voteID,
-      participantId,
-      poll_ID,
-      voteTimestamp,
-      selection
-    );
-  
-    // Increment the counter
-    this.counter++;
-  
-    // Store the vote in the world state
-    await ctx.stub.putState(voteID, vote.serialise());
-  
-    return vote;
-  }
-  /**
-   * Create Poll
-   *
-   * This transaction is used to create a new poll.
-   * Only an organizer can create a poll.
-   *
-   * @param ctx - The transaction context object
-   * @param options - The list of poll options
-   * @param open - The opening date of the poll
-   * @param closed - The closing date of the poll
-   * @returns The created poll
-   */
-  async createPoll(ctx, options, open, closed) {
-    let identity = ctx.clientIdentity;
+		// Get the participant ID
+		const participantId = identity.getAttributeValue("id");
 
-    // Check if the participant is an organizer
-    if (!this.isOrganizer(identity)) {
-      throw new Error("Only voters can create votes");
-    }
+		if (!participantId) {
+			throw new Error("Participant ID not found");
+		}
 
-    // Generate a unique poll ID
-    this.counter++;
+		// Get the poll from the world state
+		const pollBuffer = await ctx.stub.getState(poll_ID);
 
-    const pollID = "poll:"+this.counter;
+		if (!pollBuffer || pollBuffer.length === 0) {
+			throw new Error(`Poll with ID ${poll_ID} does not exist`);
+		}
 
-    // Create the poll object
-    const poll = new Poll(pollID, options, open, closed);
+		const poll = JSON.parse(pollBuffer.toString());
 
-    await ctx.stub.putState(pollID, poll.serialise());
-    // Store the poll in the world state
+		// Check if the poll is open
+		const currentTimestamp = new Date().getTime();
+		const openTimestamp = new Date(poll.open).getTime();
+		const closedTimestamp = new Date(poll.closed).getTime();
 
-    return JSON.stringify(poll);
-  }
+		if (currentTimestamp < openTimestamp) {
+			throw new Error("The poll is not open yet");
+		}
+
+		if (currentTimestamp > closedTimestamp) {
+			throw new Error("The poll is closed");
+		}
+
+		// Generate a unique vote ID
+		const voteID = "vote:" + this.counter;
+
+		// Create the vote object
+		const vote = new Vote(voteID, participantId, poll_ID, voteTimestamp, selection);
+
+		// Increment the counter
+		this.counter++;
+
+		// Store the vote in the world state
+		await ctx.stub.putState(voteID, vote.serialise());
+
+		return vote;
+	}
+	/**
+	 * Create Poll
+	 *
+	 * This transaction is used to create a new poll.
+	 * Only an organizer can create a poll.
+	 *
+	 * @param ctx - The transaction context object
+	 * @param options - The list of poll options
+	 * @param open - The opening date of the poll
+	 * @param closed - The closing date of the poll
+	 * @returns The created poll
+	 */
+	async createPoll(ctx, options, open, closed) {
+		let identity = ctx.clientIdentity;
+
+		// Check if the participant is an organizer
+		if (!this.isOrganizer(identity)) {
+			throw new Error("Only voters can create votes");
+		}
+
+		// Generate a unique poll ID
+		this.counter++;
+
+		const pollID = "poll:" + this.counter;
+
+		// Create the poll object
+		const poll = new Poll(pollID, options, open, closed);
+
+		const pollData = {
+			...poll,
+			options: options, // Store the options as array
+		};
+
+		await ctx.stub.putState(pollID, JSON.stringify(poll)); // Store the serialized pollData object in the world state
+
+		return JSON.stringify(poll);
+	}
 }
-
 
 module.exports = EggTrackingContract;
